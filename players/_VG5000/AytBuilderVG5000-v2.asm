d857136a699060f32d5fd873bf39d2469a394527
Serge Querné <logon.system@free.fr>
2025-10-09 10:53:34 +0200

    Add files via upload

--- players/_VG5000/AytBuilderVG5000-v2.asm
+++ players/_VG5000/AytBuilderVG5000-v2.asm
@@ -240,11 +240,11 @@ endif
 		; 
 		ld l,(ix+AYT_OFS_ListInit)	; Offset on Init List 
 		ld h,(ix+AYT_OFS_ListInit+1)
-		ld a,h				; If Offset=0 no Init List
-		or l
-		ld (Ayt_InitCreate),a
 		add hl,bc			; Ptr on Ay Init List 
 		ld (Ayt_PtrInitList),hl		; In Block 4 if created
+		ld a,(hl)			; If empty list, initcode=ret
+		ld (Ayt_InitCreate),a
+
 		;
 		ld l,(ix+AYT_OFS_LoopSeq)	; Offset of Loop Sequence
 		ld h,(ix+AYT_OFS_LoopSeq+1)
@@ -424,7 +424,7 @@ Ayt_InitDefByBuilder
 		ld (hl),#c9			; Init routine default off
 Ayt_InitCreate	equ $+1
 		ld a,0				; Ay Reg to init ?
-		or a
+		inc a				; Empty list if 0xff
 		ret z				; No (14 register or non available) & give ptr
 		ld hl,Ayt_Player_B4_Start
 		ld bc,AYT_B4_SIZE		; block 4 created
