64bbfb7723a82803574406ca04007a5d3c1764ab
Serge Querné <logon.system@free.fr>
2025-10-24 21:16:22 +0200

    Add files via upload

--- players/MSX/AytPlayerBuilder-MSX.asm
+++ players/MSX/AytPlayerBuilder-MSX.asm
@@ -168,9 +168,7 @@
 ;; In development, if the return address is a label that varies, this forces the presence of the Ayt_Builder in memory at each compilation.
 ;;
 ;;
-	ifndef
-	 	PlayerAccessByJP	equ 0		; If 1, requires you to take into account that SP has been wildly modified
-	endif
+PlayerAccessByJP	equ 1		; If 1, requires you to take into account that SP has been wildly modified
 ;;----------------------------------------------------------------------------------------------------------------------------------------------
 
 ;-----------------------------------------------------------------------------------------------------------------------------------------------
